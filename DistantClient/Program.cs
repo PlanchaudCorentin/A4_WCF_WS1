67db62393196cf6a72f0b8bea185299cff7f6abc
Corentin <corentin.planchaud@viacesi.fr>
2019-06-11 23:44:57 +0200

    exo 3

---
+++ DistantClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DistantClient.DispatchingService;
+
+namespace DistantClient
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DispatchingClient factory = new DispatchingClient("DispatchingService");
+            IDispatching service = factory;
+            try
+            {
+
+                MSG addParam = new MSG();
+                addParam.AppToken = "tok";
+                addParam.DataTable = new Object[] {4, 5};
+                addParam.OpName = "add";
+                MSG add = service.dispatch(addParam);
+                //MSG sub = service.dispatch(new MSG("sub", "tok", new Object[] {4, 5}));
+                //MSG operror = service.dispatch(new MSG("div", "tok", new Object[] {1, 2}));
+                //MSG tok_error = service.dispatch(new MSG("add", "wrongtok", new Object[] {1, 2}));
+                Console.WriteLine(add.DataTable[0]);
+                //Console.WriteLine(sub.DataTable[0]);
+                //Console.WriteLine(operror.DataTable[0]);
+                //Console.WriteLine(tok_error.DataTable[0]);
+                
+            }
+            catch
+            {
+                factory.Abort();
+                throw;
+            }
+            factory.Close();
+            
+        }
+    }
+}
---
+++ DistantClient2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DistantClient2.DispatchingService;
+
+namespace DistantClient2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MSG msg = new MSG();
+            msg.AppToken = "tok";
+            msg.DataTable = new Object[] {390, 84};
+            msg.OpName = "sub";
+            Console.WriteLine(new DispatchingService.DispatchingClient().dispatch(msg).DataTable[0]);
+        }
+    }
+}
